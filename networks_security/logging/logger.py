0378fe6108f66846e670d2851f7411073925dbb6
Yash <sj9399037128@gmail.com>
2026-03-15 14:45:37 +0530

    logging and exception handling

---
+++ networks_security/logging/logger.py
@@ -0,0 +1,17 @@
+import logging 
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+logs_path = os.path.join(os.getcwd(), 'logs', LOG_FILE)
+os.makedirs(os.path.dirname(logs_path), exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(os.getcwd(), 'logs', LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
